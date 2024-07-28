efa19133445452ac9b99486f6ffb470e5dcf87c8
Souvik017 <souvikkdas@gamil.com>
2024-07-29 01:58:51 +0530

    animation 1 done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -8,35 +8,12 @@ import { MotionPathPlugin } from "gsap/all";
 const Game = () => {
   
   
-  const boat1 = useRef();
+  const boat1_1 = useRef();
+  const boat1_2 = useRef();
+  const boat1_3 = useRef();
   const { contextSafe } = useGSAP();
   
-  // const moveBoat = contextSafe(() => {
-  //   const tl = gsap.timeline();
-  
-  //   tl.to(boat1.current, {
-  //     y: -100,
-  //     rotate:-20,
-  //     x:60,
-  //     duration: 2
-  //   })
-  //   .to(boat1.current, {
-  //     rotate:-20,
-  //     x: 980,
-  //     duration: 6
-  //   })
-  //   .to(boat1.current, {
-  //     rotate:20,
-     
-  //     y: 80,
-  //     x: 900,
-  //     duration: 4
-  //   })
-  //   .to(boat1.current, {
-  //     x: 830,
-  //     duration: 2
-  //   });
-  // });
+
 
   gsap.registerPlugin(MotionPathPlugin);
 
@@ -44,43 +21,110 @@ const moveBoat = contextSafe(() => {
   const tl = gsap.timeline();
 
   // Define the motion path
-  const path = [
+  const path1 = [
     { x: 0, y: -2 },
     { x: 0, y: -5 },
-    { x: 0, y: -50 },
-    { x: 980, y: -10 },
-    { x: 900, y: 80 },
-    { x: 830, y: 80 }
+    { x: 0, y: -20 },
+    { x: 50, y: -70 },
+    { x: 70, y: -70 },
   ];
 
-  tl.to(boat1.current, {
-    duration: 14,
-    motionPath: {
-      path: path,
-      autoRotate: true, // Automatically rotate the element along the path
-      curviness: 1, // Adjust the curviness of the path
-    },
+  const path2 = [
+    { x: 100, y: -80 },
+    { x: 120, y: -80 },
+    { x: 130, y: -80 },
+    { x: 150, y: -80 },
+  ];
+
+  const path3 = [
+    { x: 150, y: 0 },
+    { x: 300, y: 0 },
+    { x: 500, y: 0 },
+  ];
+
+  const path4 = [
+    { x: 30, y: 10 },
+    { x: 10, y: 40 },
+    { x: -20, y: 60 },
+    { x: -50, y: 80 },
+    { x: -90, y: 120 },
+  ];
+
+  tl.to(boat1_1.current, {
+    duration: 1,
+    rotate: -80,
     ease: "power1.inOut"
   });
+ tl.to(boat1_1.current, {
+    duration: 1,
+    motionPath: {
+      path: path1,
+      curviness: 1
+    },
+    rotate:-30,
+     ease: "power1.inOut"
+  });
+  tl.to(boat1_1.current, {
+    duration: 1,
+    motionPath: {
+      path: path2,
+      curviness: 1
+    },
+    opacity:0,
+    rotate:-20,
+    
+    ease: "power1.inOut"
+  });
+ 
+  // Animate the remaining path without changing rotation
+
+
+  tl.to(boat1_2.current, 
+    { 
+      duration: 3,
+      motionPath: {
+        path: path3,
+        curviness: 1
+      },
+      opacity:0,
+      ease: "power1.inOut"
+    }
+  );
+
+  tl.to(boat1_3.current, 
+    { 
+      duration: 3,
+      motionPath: {
+        path: path4,
+        curviness: 1
+      },
+      ease: "power1.inOut"
+    }
+  );
+
 });
 
   return (
-<div className="w-full h-screen flex flex-col justify-center items-center gap-4 relative bg-[#D7F4FD]">
+    <div>
+<div className="w-full h-screen flex flex-col justify-center items-center gap-4 relative bg-[#D7F4FD] border-3 border-red-600">
 
-<div className="Game w-[70%] h-[85%] border-black ">
-
-<div className="Boat w-20 h-[4.6rem] absolute top-20" ref={boat1}></div>
+<div className="Game w-[70%] h-[90%]">
+<div className="Boat w-20 h-[4.6rem] absolute top-[-2%] left-[45%] rotate-[-16deg]" ref={boat1_2}></div>
+<div className="Boat w-20 h-[4.6rem] absolute top-[10%]" ref={boat1_1}></div>
+<div className="Boat2 w-20 h-[4.6rem] absolute top-[8%] right-[10%]" ref={boat1_3}></div>
 {/* <div className="Boat w-20 h-[4.6rem]"></div>
 <div className="Boat w-20 h-[4.6rem]"></div>
 <div className="Boat w-20 h-[4.6rem]"></div> */}
 
 </div>
 
-<div>
-  <button onClick={moveBoat}
-  className="border-black border p-4">move</button>
-</div>
+
     </div>
+    <div>
+    <button onClick={moveBoat}
+    className="border-black border p-4">move</button>
+  </div>
+  </div>
    
   )
 }

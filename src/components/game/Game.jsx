5f75a238d370df89df5b757cf86edf35459948d7
Souvik017 <souvikkdas@gamil.com>
2024-08-01 18:53:12 +0530

    animation done

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -5,7 +5,7 @@ import { MotionPathPlugin } from "gsap/all";
 
  
 
-const Game = ({ move1, setMove1, move2, setMove2 , move3, setMove3}) => {
+const Game = ({ move1, setMove1}) => {
 
   const [reset, setReset] = useState(false)
   
@@ -199,97 +199,97 @@ const Game = ({ move1, setMove1, move2, setMove2 , move3, setMove3}) => {
 
 // })
 
-const moveBoatBottom = contextSafe(() => {
+// const moveBoatBottom = contextSafe(() => {
 
-  const t3 = gsap.timeline({
-   onComplete: () => {
-     gsap.delayedCall(1, () => {
-       gsap.set(boat1_5.current, { clearProps: 'all' });
-       gsap.set(boat1_6.current, { clearProps: 'all' });
-     });
-   }
- });
+//   const t3 = gsap.timeline({
+//    onComplete: () => {
+//      gsap.delayedCall(1, () => {
+//        gsap.set(boat1_5.current, { clearProps: 'all' });
+//        gsap.set(boat1_6.current, { clearProps: 'all' });
+//      });
+//    }
+//  });
 
- const path1 = [
-  { x: 0, y: 0 },
-  { x: -10, y: 5 },
-  { x: -20, y: 10 },
-  { x: -120, y: 5 }
-];
+//  const path1 = [
+//   { x: 0, y: 0 },
+//   { x: -10, y: 5 },
+//   { x: -20, y: 10 },
+//   { x: -120, y: 5 }
+// ];
 
-const path2 = [
-  { x: -120, y: 5 },
-  { x: -140, y: 7 },
-  { x: -150, y: 10 },
-  { x: -200, y: 15 }
-];
+// const path2 = [
+//   { x: -120, y: 5 },
+//   { x: -140, y: 7 },
+//   { x: -150, y: 10 },
+//   { x: -200, y: 15 }
+// ];
 
-const path3 = [
-  { x: -120, y: 0 },
-  { x: -140, y: 2 },
-  { x: -150, y: 4 },
-];
+// const path3 = [
+//   { x: -120, y: 0 },
+//   { x: -140, y: 2 },
+//   { x: -150, y: 4 },
+// ];
 
-const path4 = [
-  { x: -180, y: 5 },
-  { x: -200, y: 5 },
-];
+// const path4 = [
+//   { x: -180, y: 5 },
+//   { x: -200, y: 5 },
+// ];
 
-const path5 = [
-  { x: 0, y: -10 },
-  { x: 5, y: -20 },
-  { x: 10, y: -30 },
-  { x: 20, y: -60 },
-  { x: 40, y: -90 },
-];
+// const path5 = [
+//   { x: 0, y: -10 },
+//   { x: 5, y: -20 },
+//   { x: 10, y: -30 },
+//   { x: 20, y: -60 },
+//   { x: 40, y: -90 },
+// ];
 
- t3.to(boat1_5.current, {
-  duration: 3,
-  motionPath: {
-    path: path1,
-    curviness: 1
-  },
-  ease: "power1.inOut"
-}).to(boat1_5.current, {
-  duration: 3,
-  motionPath: {
-    path: path2,
-    curviness: 1
-  },
-  opacity:0,
-  ease: "power1.inOut"
-})
+//  t3.to(boat1_5.current, {
+//   duration: 3,
+//   motionPath: {
+//     path: path1,
+//     curviness: 1
+//   },
+//   ease: "power1.inOut"
+// }).to(boat1_5.current, {
+//   duration: 3,
+//   motionPath: {
+//     path: path2,
+//     curviness: 1
+//   },
+//   opacity:0,
+//   ease: "power1.inOut"
+// })
 
-t3.to(boat1_6.current, {
-  duration: 3,
-  motionPath: {
-    path: path3,
-    curviness: 1
-  },
-  ease: "linear"
-}).to(boat1_6.current, {
-  duration: 3,
-  motionPath: {
-    path: path4,
-    curviness: 1
-  },
-  opacity:0,
-  ease: "power1.inOut"
-})
+// t3.to(boat1_6.current, {
+//   duration: 3,
+//   motionPath: {
+//     path: path3,
+//     curviness: 1
+//   },
+//   ease: "linear"
+// }).to(boat1_6.current, {
+//   duration: 3,
+//   motionPath: {
+//     path: path4,
+//     curviness: 1
+//   },
+//   opacity:0,
+//   ease: "power1.inOut"
+// })
 
-t3.to(boat1_7.current, {
-  duration: 3,
-  motionPath: {
-    path: path5,
-    curviness: 1
-  },
-  rotate:-80,
-  ease: "power1.inOut"
-})
+// t3.to(boat1_7.current, {
+//   duration: 3,
+//   motionPath: {
+//     path: path5,
+//     curviness: 1
+//   },
+//   rotate:-80,
+//   ease: "power1.inOut"
+// })
 
 
 
-})
+// })
 
 const moveBoat = contextSafe(() => {
 
@@ -300,6 +300,9 @@ const moveBoat = contextSafe(() => {
         gsap.set(boat1_2.current, { clearProps: 'all' });
         gsap.set(boat1_3.current, { clearProps: 'all' });
         gsap.set(boat1_4.current, { clearProps: 'all' });
+        gsap.set(boat1_5.current, { clearProps: 'all' });
+        gsap.set(boat1_6.current, { clearProps: 'all' });
+        gsap.set(boat1_7.current, { clearProps: 'all' });
       });
     }
   });
@@ -345,6 +348,39 @@ const moveBoat = contextSafe(() => {
     { x: -120, y: 5 }
   ];
   
+  const path8 = [
+    { x: 0, y: 0 },
+    { x: -10, y: 5 },
+    { x: -20, y: 10 },
+    { x: -120, y: 5 }
+  ];
+  
+  const path9 = [
+    { x: -120, y: 5 },
+    { x: -140, y: 7 },
+    { x: -150, y: 10 },
+    { x: -200, y: 15 }
+  ];
+  
+  const path10 = [
+    { x: -120, y: 0 },
+    { x: -140, y: 2 },
+    { x: -150, y: 4 },
+  ];
+  
+  const path11 = [
+    { x: -180, y: 5 },
+    { x: -200, y: 5 },
+  ];
+  
+  const path12 = [
+    { x: 0, y: -10 },
+    { x: 5, y: -20 },
+    { x: 10, y: -30 },
+    { x: 20, y: -60 },
+    { x: 40, y: -90 },
+  ];
+  
 
   tl.to(boat1_1.current, {
     duration: 3,
@@ -374,7 +410,6 @@ const moveBoat = contextSafe(() => {
     ease: "power1.inOut",
   });
 
-
   tl.to(boat1_2.current, { 
     duration: 5, // Total duration for the combined path
     motionPath: {
@@ -393,20 +428,6 @@ const moveBoat = contextSafe(() => {
   });
 
 
-
-  tl.to(boat1_3.current, 
-    { 
-      duration: 3,
-      motionPath: {
-        path: path4,
-        curviness: 1
-      },
-      onComplete: () => {
-       setMove1(false); 
-     },
-      ease: "power1.inOut"
-    }
-  );
 
   tl.to(boat1_3.current, {
     duration: 3,
@@ -436,20 +457,53 @@ const moveBoat = contextSafe(() => {
     ease: "power1.inOut"
   })
   
+ 
+
   tl.to(boat1_5.current, {
     duration: 3,
     motionPath: {
-      path: path7,
+      path: path8,
       curviness: 1
     },
-    rotate: 10,
     ease: "power1.inOut"
   }).to(boat1_5.current, {
-    duration: 1,
-    onComplete: () => {
-      setMove2(false); 
+    duration: 3,
+    motionPath: {
+      path: path9,
+      curviness: 1
     },
+    opacity:0,
     ease: "power1.inOut"
+  })
+  
+  tl.to(boat1_6.current, {
+    duration: 3,
+    motionPath: {
+      path: path10,
+      curviness: 1
+    },
+    ease: "linear"
+  }).to(boat1_6.current, {
+    duration: 3,
+    motionPath: {
+      path: path11,
+      curviness: 1
+    },
+    opacity:0,
+    ease: "power1.inOut"
+  })
+  
+  tl.to(boat1_7.current, {
+    duration: 3,
+    motionPath: {
+      path: path12,
+      curviness: 1
+    },
+    rotate:-80,
+    ease: "power1.inOut",
+    onComplete: () => {
+      setMove1(false);
+    }
   })
   
 
@@ -460,11 +514,9 @@ useEffect(() => {
 
     if(move1){
    moveBoat();
-  //  moveBoatRight()
-  //  moveBoatBottom()
     }
  
-}, [move1, move2, move3])
+}, [move1])
 
 
 console.log(reset)
